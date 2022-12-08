ad199d0cb11e18c8d3046aad214d89a491878beb
Cker <robin147@seznam.cz>
2022-12-08 23:01:36 +0100

    06 Done

---
+++ 06A.py
@@ -0,0 +1,32 @@
+# Signals
+
+
+def openFile():
+    return open("06Input", "r")
+
+
+def forLine(file):
+    lines = file.readlines()
+    charactersLoadedCount = 0
+    array = []
+    arrayLoaded = False
+
+    for line in lines:
+        for character in line:
+
+            # Loading array
+            if not arrayLoaded:
+                arrayLoaded = True if charactersLoadedCount == 4 else False
+
+            else:
+                # Check if unique
+                if len(set(array)) == len(array):
+                    return charactersLoadedCount - 1
+                # Load new values
+                array.pop(0)
+
+            array.append(character)
+            charactersLoadedCount += 1
+
+
+print(forLine(openFile()))
