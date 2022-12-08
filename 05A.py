aa7f95afa8749ea0f93f320c59b6db0b4f521cde
Cker <robin147@seznam.cz>
2022-12-08 01:38:04 +0100

    05 Init + try (Stuck on not loading first line/index ofr)

---
+++ 05A.py
@@ -0,0 +1,42 @@
+# Crane operator
+
+
+def openFile():
+    return open("05Input", "r")
+
+
+def forLine(file):
+    lines = file.readlines()
+    ret = ''
+    linesTemp = []
+    cratePositionsLoaded = False
+    cratePositions = [[], [], [], [], [], [], [], [], []]
+    for line in lines:
+        if line.strip() and not cratePositionsLoaded:
+            linesTemp.append(line)
+        elif not cratePositionsLoaded:
+            cratePositionsLoaded = True
+            # Loading the arrays of crate positions
+            for stack in range(0, 9):
+                pos = 1 + stack * 4
+                for i in range(len(linesTemp) - 2, 0, -1):
+                    if linesTemp[i][pos] != ' ':
+                        cratePositions[stack].append(linesTemp[i][pos])
+                        print(cratePositions)
+        elif cratePositionsLoaded:
+            # Moving the crates
+            orders = line.rstrip().split(" ")
+            for i in range(0, int(orders[1])):
+                if not cratePositions[int(orders[3])-1]:
+                    cratePositions[int(orders[5])-1].append(cratePositions[int(orders[3])-1][-1])
+                    cratePositions[int(orders[3])-1].pop()
+
+    print(cratePositions)
+    # Final
+    for stack in range(0, 9):
+        ret += cratePositions[stack][-1]
+
+    return ret
+
+
+print(forLine(openFile()))
