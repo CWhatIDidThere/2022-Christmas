6db0ec41e89aecfdcc16637ad1907d88456fd944
Cker <robin147@seznam.cz>
2022-12-09 11:06:10 +0100

    07A Progress - will delete all and start again
    - Cant do it like this two dimentional as there can be duplicates

--- 07A.py
+++ 07A.py
@@ -20,7 +20,9 @@ def forLine(file):
 
     for line in lines:
         # Lexical + Syntax
+        line = line.rstrip()
         splitLine = line.split(" ")
+
         if splitLine[0] == '$':
             if splitLine[1] == 'cd':
                 if splitLine[2] == '/':
@@ -34,16 +36,24 @@ def forLine(file):
                 skipIsRealIDontNeedThisLSCheckAsITrustThemSoNoSyntaxCheck = True
 
         elif splitLine[0] == 'dir':
-            if splitLine[1] in dick:
+            print('FOUND DIR ' + splitLine[1])
+            if currentDict[-1] in dick:
                 # Add new dir under this dir
+                # This is a hack. Not sure why it works like this
+                if dick[currentDict[-1]][2] is None:
+                    dick[currentDict[-1]][2] = []
                 if splitLine[1] not in dick[currentDict[-1]][2]:
-                    dick[splitLine[1]] = dick[currentDict[-1]][2].append(splitLine[1])
+                    dick[splitLine[1]] = [dick[currentDict[-1]][0], dick[currentDict[-1]][1], dick[currentDict[-1]][2].append(splitLine[1]), dick[currentDict[-1]][3]]
             # Create new
             else:
-                dick[splitLine[1]] = [False, 0, [splitLine[1]], []]
+                dick[currentDict[-1]] = [False, 0, [splitLine[1]], []]
+                print('NEW DIR')
+                print(currentDict[-1])
+                print(dick[currentDict[-1]])
         # FILE
         else:
-            if splitLine[1] in dick:
+            print('FOUND FILE ' + splitLine[1])
+            if currentDict[-1] in dick:
                 # Add new file under this dir
                 if splitLine[1] not in dick[currentDict[-1]][3]:
                     # TODO Check if legit!!!
@@ -51,7 +61,10 @@ def forLine(file):
                     dick[splitLine[1]] = dick[currentDict[-1]][1] + int(splitLine[0])
             # Create new
             else:
-                dick[splitLine[1]] = [False, int(splitLine[0]), [], [splitLine[1]]]
+                dick[currentDict[-1]] = [False, int(splitLine[0]), [], [splitLine[1]]]
+                print('NEW FILE + DIR')
+                print(currentDict[-1])
+                print(dick[currentDict[-1]])
 
     return dick
 
