29e06db12c48fb376cfba9668848a2260634b68c
Cker <robin147@seznam.cz>
2022-12-09 17:15:22 +0100

    07A - I surrender for now

--- 07A.py
+++ 07A.py
@@ -1,5 +1,6 @@
 # Parser
-
+from enum import Enum
+global lessThan100000Size
 
 def openFile():
     return open("07Input", "r")
@@ -13,6 +14,12 @@ def openFile():
 # DirName[1]: Calculated size (if [0] is true then this is final size)
 # DirName[2]: Array of dictionaries of the directories contained
 # DirName[3]: Array of stings of names of the files contained
+
+class D(Enum):
+    Bool = 0
+    Size = 1
+    Dir = 2
+    File = 3
 
 
 def forLine(file):
@@ -37,7 +44,7 @@ def forLine(file):
                 lsCalled = True
 
         elif splitLine[0] == 'dir':
-            print('FOUND DIR ' + splitLine[1])
+            # print('FOUND DIR ' + splitLine[1])
             if currentDict[-1] in dick:
                 # Add new dir under this dir
                 # This is a hack. Not sure why it works like this
@@ -49,14 +56,14 @@ def forLine(file):
                 if len(dick[currentDict[-1]][2]) == 0:
                     dick[currentDict[-1]][2] = {}
                 if splitLine[1] not in dick[currentDict[-1]][2]:
-                    dick[currentDict[-1]][2][str(splitLine[1])] = [False, 0, [], []]
+                    dick[currentDict[-1]][2][str(splitLine[1])] = [False, 0, {}, []]
             # Create new
             else:
-                dick[currentDict[-1]] = [False, 0, {splitLine[1]: [False, 0, [], []]}, []]
-                print('NEW DIR')
+                dick[currentDict[-1]] = [False, 0, {splitLine[1]: [False, 0, {}, []]}, []]
+                # print('NEW DIR')
         # FILE
         else:
-            print('FOUND FILE ' + splitLine[1])
+            # print('FOUND FILE ' + splitLine[1])
             if currentDict[-1] in dick:
                 # Add new file under this dir
                 if splitLine[1] not in dick[currentDict[-1]][3]:
@@ -66,12 +73,13 @@ def forLine(file):
             # Create new
             else:
                 dick[currentDict[-1]] = [False, int(splitLine[0]), {}, [splitLine[1]]]
-                print('NEW FILE + DIR')
+                # print('NEW FILE + DIR')
 
+    print('-------Rec-------')
     # Binary tree count
-    recursionWillSaveUsRight(dick)
+    recursionWillSaveUsRight(dick['/'])
 
-    return dick
+    return lessThan100000Size
 
 # TODO WE GET NEW FOLDER - NAME + MY MASTER FOLDER
 # TODO WE CREATE OUR STRUCT FOR THIS FOLDER
@@ -81,8 +89,17 @@ def forLine(file):
 lessThan100000Size = 0
 
 
-def recursionWillSaveUsRight(dick):
-    print('YES')
+def recursionWillSaveUsRight(recDick):
+    print(recDick)
+    print(recDick[D.Dir])
+    print(recDick.keys())
+    for folder in recDick[D.Dir].keys():
+        recDick[D.Size] += recursionWillSaveUsRight(recDick[D.Dir][folder])
+
+    recDick[D.Bool] = True
+    if recDick[D.Size] > 100000:
+        lessThan100000Size += 1
+    return recDick[D.Size]
 
 
 print(forLine(openFile()))
